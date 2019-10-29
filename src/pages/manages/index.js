9216429da2d4a7d77b56f615664df60f903484f9
moming <2235868313@qq.com>
2019-10-29 19:42:13 +0800

    项目init

---
+++ src/pages/manages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Manage extends Component {
+  render() {
+    return (
+      <div>
+        <p>后台系统</p>
+      </div>
+    )
+  }
+}
+export default Manage
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+  name: 'zzz'
+}
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}

fda9da8e4996eaf752f723d6cd74e79c78951023
moming <2235868313@qq.com>
2019-11-01 22:18:56 +0800

    完善路由跳转

--- src/pages/manages/index.js
+++ src/pages/manages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Layout, Breadcrumb, Avatar, Menu, Icon } from 'antd'
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import Home from './Home'
 import Explanation from './Explanation'
@@ -13,7 +13,7 @@ import AdminList from './AdminList'
 import OrderList from './OrderList'
 import UserList from './UserList'
 import FoodList from './FoodList'
-import ShopList from './shopList'
+import ShopList from './ShopList'
 
 import './index.scss'
 
@@ -21,26 +21,56 @@ const { Header, Sider, Content } = Layout
 const { SubMenu } = Menu
 
 class Manage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      breadcrumb: [],
+      clickKey: ''
+    }
+  }
   handleClick = e => {
-    console.log('click ', e)
+    if (e.key === this.state.clickKey) return
+    let breadcrumb = []
+    if (e.keyPath.length === 1) {
+      breadcrumb = ['首页']
+    } else {
+      let keyPath = e.keyPath.reverse()
+      keyPath.unshift('首页')
+      breadcrumb = keyPath
+    }
+    this.setState({
+      breadcrumb,
+      clickKey: e.key
+    })
   }
   render() {
+    let { clickKey, breadcrumb } = this.state
     return (
       <Layout className="manage">
-        <Sider className="sider-bar">
+        <Sider
+          className="sider-bar"
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0
+          }}
+        >
           <Menu
             onClick={this.handleClick}
-            defaultSelectedKeys={['0']}
-            defaultOpenKeys={[]}
+            selectedKeys={[clickKey]}
+            openKeys={[breadcrumb[1]]}
             mode="inline"
             theme="dark"
           >
-            <Menu.Item key="0">
-              <Icon type="appstore" theme="filled" />
-              首页
+            <Menu.Item key="首页">
+              <NavLink to="/manage">
+                <Icon type="appstore" theme="filled" />
+                首页
+              </NavLink>
             </Menu.Item>
             <SubMenu
-              key="sub1"
+              key="数据管理"
               title={
                 <span>
                   <Icon type="file" />
@@ -48,14 +78,24 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="1">用户列表</Menu.Item>
-              <Menu.Item key="2">商家列表</Menu.Item>
-              <Menu.Item key="3">食品列表</Menu.Item>
-              <Menu.Item key="4">订单列表</Menu.Item>
-              <Menu.Item key="5">管理员列表</Menu.Item>
+              <Menu.Item key="用户列表">
+                <NavLink to="/manage/userList">用户列表</NavLink>
+              </Menu.Item>
+              <Menu.Item key="商家列表">
+                <NavLink to="/manage/shopList">商家列表</NavLink>
+              </Menu.Item>
+              <Menu.Item key="食品列表">
+                <NavLink to="/manage/foodList">食品列表</NavLink>
+              </Menu.Item>
+              <Menu.Item key="订单列表">
+                <NavLink to="/manage/orderList">订单列表</NavLink>
+              </Menu.Item>
+              <Menu.Item key="管理员列表">
+                <NavLink to="/manage/adminList">管理员列表</NavLink>
+              </Menu.Item>
             </SubMenu>
             <SubMenu
-              key="sub2"
+              key="添加数据"
               title={
                 <span>
                   <Icon type="plus" />
@@ -63,11 +103,15 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="6">添加商铺</Menu.Item>
-              <Menu.Item key="7">添加商品</Menu.Item>
+              <Menu.Item key="添加商铺">
+                <NavLink to="/manage/addShop">添加商铺</NavLink>
+              </Menu.Item>
+              <Menu.Item key="添加商品">
+                <NavLink to="/manage/addGood">添加商品</NavLink>
+              </Menu.Item>
             </SubMenu>
             <SubMenu
-              key="sub3"
+              key="图表"
               title={
                 <span>
                   <Icon type="pie-chart" theme="filled" />
@@ -75,10 +119,12 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="8">用户分布</Menu.Item>
+              <Menu.Item key="用户分布">
+                <NavLink to="/manage/visitor">用户分布</NavLink>
+              </Menu.Item>
             </SubMenu>
             <SubMenu
-              key="sub4"
+              key="编辑"
               title={
                 <span>
                   <Icon type="edit" theme="filled" />
@@ -86,10 +132,12 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="9">文本编辑</Menu.Item>
+              <Menu.Item key="文本编辑">
+                <NavLink to="/manage/edit">文本编辑</NavLink>
+              </Menu.Item>
             </SubMenu>
             <SubMenu
-              key="sub5"
+              key="设置"
               title={
                 <span>
                   <Icon type="setting" />
@@ -97,10 +145,12 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="10">管理员设置</Menu.Item>
+              <Menu.Item key="管理员设置">
+                <NavLink to="/manage/adminSet">管理员设置</NavLink>
+              </Menu.Item>
             </SubMenu>
             <SubMenu
-              key="sub6"
+              key="说明"
               title={
                 <span>
                   <Icon type="exclamation-circle" theme="filled" />
@@ -108,18 +158,18 @@ class Manage extends Component {
                 </span>
               }
             >
-              <Menu.Item key="11">说明</Menu.Item>
+              <Menu.Item key="说明">
+                <NavLink to="/manage/explanation">说明</NavLink>
+              </Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
-        <Layout>
+        <Layout style={{ marginLeft: 200 }}>
           <Header className="header-container">
             <Breadcrumb className="breadcrumb">
-              <Breadcrumb.Item>首页</Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <a href="#/manage">Application Center</a>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>An Application</Breadcrumb.Item>
+              {this.state.breadcrumb.map((item, index) => {
+                return <Breadcrumb.Item key={index}>{item}</Breadcrumb.Item>
+              })}
             </Breadcrumb>
             <div>
               <Avatar size="large" icon="user" className="avatar" />
@@ -146,6 +196,65 @@ class Manage extends Component {
         </Layout>
       </Layout>
     )
+  }
+  componentDidMount() {
+    let keyName = this.props.location.pathname.split('/').pop()
+    let newBreadcrumb = ['首页']
+    let key = '首页'
+    if (keyName !== 'manage') {
+      switch (keyName) {
+        case 'userList':
+          newBreadcrumb.push('数据管理', '用户列表')
+          key = '用户列表'
+          break
+        case 'shopList':
+          newBreadcrumb.push('数据管理', '商家列表')
+          key = '商家列表'
+          break
+        case 'foodList':
+          newBreadcrumb.push('数据管理', '食品列表')
+          key = '食品列表'
+          break
+        case 'orderList':
+          newBreadcrumb.push('数据管理', '订单列表')
+          key = '订单列表'
+          break
+        case 'adminList':
+          newBreadcrumb.push('数据管理', '管理员列表')
+          key = '管理员列表'
+          break
+        case 'addShop':
+          newBreadcrumb.push('添加数据', '添加商铺')
+          key = '添加商铺'
+          break
+        case 'addGood':
+          newBreadcrumb.push('添加数据', '添加商品')
+          key = '添加商品'
+          break
+        case 'visitor':
+          newBreadcrumb.push('图表', '用户分布')
+          key = '用户分布'
+          break
+        case 'edit':
+          newBreadcrumb.push('编辑', '文本编辑')
+          key = '文本编辑'
+          break
+        case 'adminSet':
+          newBreadcrumb.push('设置', '管理员设置')
+          key = '管理员设置'
+          break
+        case 'explanation':
+          newBreadcrumb.push('说明', '说明')
+          key = '说明'
+          break
+        default:
+          break
+      }
+      this.setState({
+        breadcrumb: newBreadcrumb,
+        clickKey: key
+      })
+    }
   }
 }
 export default Manage
